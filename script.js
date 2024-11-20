5edb2fb4f8b7c941b269ac1daf590247a3a7e45c
arthurcrodri <arthur.sbt@yahoo.com>
2024-11-20 16:04:28 -0300

    Added function to keep track of previous page theme

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function toggleMode() {
-  const html = document.documentElement
+const html = document.documentElement
 
+function loadPreviousMode() {
+  if (localStorage.getItem("pageTheme") === "light") {
+    html.classList.toggle("light")
+  }
+}
+
+function toggleMode() {
   html.classList.toggle("light")
+  if (html.classList.contains("light")) {
+    localStorage.setItem("pageTheme", "light")
+  } else {
+    localStorage.setItem("pageTheme", "dark")
+  }
 }
