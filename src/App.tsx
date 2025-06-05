99cd74691d7aa766293fbbb52e0d2676f8ab3032
Arthur Rodrigues <arthur.rodrigues.dev@proton.me>
2025-06-05 14:56:15 -0300

    feat: configuração de rotas do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
-import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+
+import HomePage from './pages/homepage'
+import AboutPage from './pages/aboutpage'
+import ExperiencesPage from './pages/experiencespage'
+import SkillsPage from './pages/skillspage'
+import ProjectsPage from './pages/projectspage'
+
 import './App.css'
 
 function App() {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/sobre">Sobre</Link>
-          </li>
-        </ul>
-      </nav>
-
-      <hr />
-
-      {/* Define your routes here */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/sobre" element={<AboutPage />} />
-        {/* <Route path="/outro" element={<OtherPage />} /> */}
+        <Route path="/experiencias" element={<ExperiencesPage />} />
+        <Route path="/habilidades" element={<SkillsPage />} />
+        <Route path="/projetos" element={<ProjectsPage />} />
       </Routes>
-    </div>
   )
 }
 
